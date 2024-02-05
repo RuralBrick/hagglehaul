e40d6f6d1da00d3d8312e850c1638e5472449e91
CE201710 <148310909+CE201710@users.noreply.github.com>
2024-02-05 00:15:48 -0500

    Redid changes on last Wednesday
    Redid changes on last Wednesday, but it works on top of the other changes made

--- hagglehaul.client/src/App.jsx
+++ hagglehaul.client/src/App.jsx
@@ -1,56 +1,54 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TripsPage from './pages/TripsPage/TripsPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import ActivityPage from './pages/ProfilePage/ActivityPage/ActivityPage';
+import WalletPage from './pages/ProfilePage/WalletPage/WalletPage';
+import MessagesPage from './pages/ProfilePage/MessagesPage/MessagesPage';
+import SettingsPage from './pages/ProfilePage/SettingsPage/SettingsPage';
+import Signin from './Signin';  
+import Signup from './Signup';  
 import './App.css';
 
 function App() {
-    const [forecasts, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-        checkMongo();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
     return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/" style={{ fontFamily: 'Inika' }}>HaggleHaul</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav ms-auto">
+                                <Link className="nav-link" to="/trips">My Trips</Link>
+                                <Link className="nav-link" to="/profile">Profile</Link>
+                                {/* ... other navigation links */}
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <main className="pt-5">
+                    <Routes>
+                        <Route path="/" element={<TripsPage />} />
+                        <Route path="/trips" element={<TripsPage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route path="/profile/activity" element={<ActivityPage />} />
+                        <Route path="/profile/wallet" element={<WalletPage />} />
+                        <Route path="/profile/messages" element={<MessagesPage />} />
+                        <Route path="/profile/settings" element={<SettingsPage />} />
+                        <Route path="/signin" element={<Signin />} /> 
+                        <Route path="/signup" element={<Signup />} /> 
+          
+                        {/* ... other routes */}
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('api/weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
-    
-    async function checkMongo() {
-        const response = await fetch('api/mongotest/insecure');
-        const data = await response.json();
-        console.log(data);
-    }
 }
 
 export default App;
