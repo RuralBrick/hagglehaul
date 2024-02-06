fc9b42d1446a619799164abb9c8e335ad184e1fd
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-02-05 21:57:16 -0800

    Trip + Bid fields
    TODO: Figure out MongoDB references

---
+++ hagglehaul.Server/Models/Bid.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace hagglehaul.Server.Models
+{
+    public class Bid
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("centsAmount")]
+        public uint CentsAmount { get; set; }
+
+        // Rider (ref to rider that placed bid)
+        // Trip (ref to trip bid is placed under)
+    }
+}
---
+++ hagglehaul.Server/Models/Trip.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace hagglehaul.Server.Models
+{
+    public class Trip
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        // [maybe] Name (string)
+        // Route (ref to RouteCache)
+        // Bid list (ref to Bid)
+        // Rider that posted trip (ref to UserCore)
+        // Confirmed driver (ref to UserCore)
+    }
+}
