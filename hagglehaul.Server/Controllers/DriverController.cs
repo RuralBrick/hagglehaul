ffb44e0e8123d81f1643ab55e9c8ddb513e149c2
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-02-14 16:59:45 -0800

    Classes

---
+++ hagglehaul.Server/Controllers/DriverController.cs
@@ -0,0 +1,24 @@
+﻿using hagglehaul.Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hagglehaul.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private readonly IDriverProfileService _driverProfileService;
+        private readonly IRiderProfileService _riderProfileService;
+        private readonly ITripService _tripService;
+        private readonly IBidService _bidService;
+
+        public DriverController(IDriverProfileService driverProfileService, IRiderProfileService riderProfileService, ITripService tripService, IBidService bidService)
+        {
+            _driverProfileService = driverProfileService;
+            _riderProfileService = riderProfileService;
+            _tripService = tripService;
+            _bidService = bidService;
+        }
+    }
+}
