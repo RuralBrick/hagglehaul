608d6eda82e6037be4a6501fbb2131c8c9ec9b8d
warrickhe <43077069+warrickhe@users.noreply.github.com>
2024-02-17 15:21:42 -0800

    Rider Customization Added
    Also moved creating password hash/checking password hash to user core services.

--- hagglehaul.Server/Controllers/RiderController.cs
+++ hagglehaul.Server/Controllers/RiderController.cs
@@ -1,6 +1,9 @@
 ﻿using hagglehaul.Server.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using hagglehaul.Server.Models;
 
 namespace hagglehaul.Server.Controllers
 {
@@ -10,15 +13,67 @@ namespace hagglehaul.Server.Controllers
     {
         private readonly IRiderProfileService _riderProfileService;
         private readonly IDriverProfileService _driverProfileService;
+        private readonly IUserCoreService _userCoreService;
         private readonly ITripService _tripService;
         private readonly IBidService _bidService;
 
-        public RiderController(IRiderProfileService riderProfileService, IDriverProfileService driverProfileService, ITripService tripService, IBidService bidService)
+        public RiderController(IRiderProfileService riderProfileService, IDriverProfileService driverProfileService, IUserCoreService userCoreService, ITripService tripService, IBidService bidService)
         {
             _riderProfileService = riderProfileService;
             _driverProfileService = driverProfileService;
+            _userCoreService = userCoreService;
             _tripService = tripService;
             _bidService = bidService;
         }
+
+        [Authorize]
+        [HttpGet]
+        [Route("modifyAcc")]
+        public async Task<IActionResult> ModifyAccountDetails([FromBody] RiderUpdate riderUpdate )
+        {
+            ClaimsPrincipal currentUser = this.User;
+  
+            if (currentUser == null)
+            {
+                return BadRequest(new { Error = "Invalid User/Auth" });
+            }
+            bool changingPassword = !String.IsNullOrEmpty(riderUpdate.NewPassword);
+            if (String.IsNullOrEmpty(riderUpdate.CurrentPassword) && changingPassword)
+            {
+                return BadRequest(new { Error = "Can't make a new password" });
+            }
+            
+            //check role for error
+            var email = currentUser.FindFirstValue(ClaimTypes.Name); //name is the email
+            var role = currentUser.FindFirstValue(ClaimTypes.Role);
+            UserCore userCore = await _userCoreService.GetAsync(email);
+            
+            if (changingPassword)
+            {
+                if (_userCoreService.ComparePasswordToHash(riderUpdate.CurrentPassword, userCore.PasswordHash,userCore.Salt))
+                {
+                    return BadRequest(new { Error = "Current Password is invalid" });
+                }
+                
+            }
+            UserCore newUserCore = new UserCore();
+            newUserCore.Email = email;
+            newUserCore.Id = userCore.Id;
+            newUserCore.Name = userCore.Name;
+            newUserCore.Role = userCore.Role;
+            newUserCore.Phone = userCore.Phone;
+            if (changingPassword) {
+                _userCoreService.CreatePasswordHash(riderUpdate.NewPassword, out var newHash, out var newSalt);
+                newUserCore.PasswordHash = newHash;
+                newUserCore.Salt = newSalt;
+            } else
+            {
+                newUserCore.PasswordHash = userCore.PasswordHash;
+                newUserCore.Salt = userCore.Salt;
+            }
+            await _userCoreService.UpdateAsync(email, newUserCore);
+            return Ok();
+        }
     }
+
 }
