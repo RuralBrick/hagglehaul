1941dd20f27b5c04bc07cce00f564f330a0d7fad
CE201710 <148310909+CE201710@users.noreply.github.com>
2024-02-19 03:11:22 -0800

    Create CreateTripPage.jsx

---
+++ hagglehaul.client/src/pages/CreateTripPage/CreateTripPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreateTripForm from './CreateTripForm';
+
+function CreateTripPage() {
+    return (
+        <div>
+            <h2>Create Trip</h2>
+            <CreateTripForm />
+        </div>
+    );
+}
+
+
+
+export default CreateTripPage;
