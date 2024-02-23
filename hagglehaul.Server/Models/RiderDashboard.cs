9da82970920279cede1fe66b515f47e56aeb0e48
warrickhe <43077069+warrickhe@users.noreply.github.com>
2024-02-22 18:52:02 -0800

    Make Models

---
+++ hagglehaul.Server/Models/RiderDashboard.cs
@@ -0,0 +1,9 @@
+﻿namespace hagglehaul.Server.Models
+{
+    public class RiderDashboard
+    {
+        public List<ConfirmedRiderTrip> ConfirmedTrips { get; set; } = null!;
+        public List<UncomfirmedRiderTrip> TripsInBidding { get; set; } = null!;
+        public List<ArchivedRiderTrip> ArchivedTrips { get; set; } = null!;
+    }
+}
---
+++ hagglehaul.Server/Models/UncomfirmedRiderTrip.cs
@@ -0,0 +1,15 @@
+﻿namespace hagglehaul.Server.Models
+{
+    public class UncomfirmedRiderTrip
+    {
+        public string TripID { get; set; } = null!;
+        public string TripName { get; set; } = null!;
+        public byte[] Thumbnail { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public uint Distance { get; set; } //distance in meters
+        public uint Duration { get; set; } //duration in seconds
+        public List<Bid>? Bids { get; set; }
+        public string PickupAddress { get; set; } = null!;
+        public string DestinationAddress { get; set; } = null!;
+    }
+}
