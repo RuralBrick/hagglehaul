080302ba947c4b57beaa8727e13150a248831f76
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2024-02-26 07:14:50 -0800

    Add YourBid field to BidUserView.cs

--- hagglehaul.Server/Models/BidUserView.cs
+++ hagglehaul.Server/Models/BidUserView.cs
@@ -3,6 +3,7 @@
     public class BidUserView
     {
         public string? BidId { get; set; }
+        public bool YourBid { get; set; }
         public string DriverName { get; set; } = null!;
         public double? DriverRating { get; set; }
         public uint? DriverNumRating { get; set; }
