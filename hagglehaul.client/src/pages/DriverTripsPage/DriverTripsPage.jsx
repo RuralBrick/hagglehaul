dd02667562b926d0a8f245075309a1b9bb76105c
aomurtagh <156570378+aomurtagh@users.noreply.github.com>
2024-02-25 19:06:47 -0800

    Altered RiderTripPage to contain relevant TripCards

--- hagglehaul.client/src/pages/DriverTripsPage/DriverTripsPage.jsx
+++ hagglehaul.client/src/pages/DriverTripsPage/DriverTripsPage.jsx
@@ -19,36 +19,18 @@ function DriverTripsPage() {
                     <TripCard
                         image="https://placeholder.co/600x400.png"
                         title="Disneyland Park"
-                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Go somewhere</Button>}
-                        attributes={[["Column 1", "Some longer information is here :)"], ["Column 2", "Information 2"], ["$39.99", "10.2 miles"]]}
-                        bidComponents={[
-                            <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
-                            </Row>,
-                            <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
-                            </Row>,
-                            <Button variant="light">+ Add Bid</Button>]}
-                    />
-                    <TripCard
-                        image="https://placeholder.co/600x400.png"
-                        title="Staples Center"
-                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Go somewhere</Button>}
-                        attributes={[["Column 1", "Information 1"], ["Column 2", "Information 2"], ["Column 3", "Information 3"]]}
-                        bidComponents={[]}
-                    />
-                    <TripCard
-                        image="https://placeholder.co/600x400.png"
-                        title="UCLA Jules Stein Eye Institute"
                         actionComponent={<DropdownButton as={ButtonGroup} title="Options" id="bg-nested-dropdown"
                             variant="light">
                             <Dropdown.Item>Cancel Trip</Dropdown.Item>
-                            <Dropdown.Item>Do Something</Dropdown.Item>
+                            <Dropdown.Item>Show More</Dropdown.Item>
                         </DropdownButton>}
-                        attributes={[["Column 1", "Information 1"], ["Column 2", "Information 2"], ["Column 3", "Information 3"]]}
-                        bidComponents={[]}
+                        attributes={[["Mon, Feb 19, 08:00 AM", "12 miles - 36 minutes"], ["Tim Pajim", "4.5\u2605 (130)"], ["$39.99", ""]]}
+                        bidComponents={[
+                            <Row>
+                                <Col
+                                    style={{ display: 'flex', justifyContent: 'center' }}> <span style={{ fontSize: "1.5em" }}>Rate Driver: &#x2606;&#x2606;&#x2606;&#x2606;&#x2606; </span>
+                                </Col>
+                            </Row>]}
                     />
                 </Row>
                 <div className="trips-header mt-4 mb-4">
--- hagglehaul.client/src/pages/RiderTripsPage/RiderTripsPage.jsx
+++ hagglehaul.client/src/pages/RiderTripsPage/RiderTripsPage.jsx
@@ -21,36 +21,18 @@ function RiderTripsPage() {
                     <TripCard
                         image="https://placeholder.co/600x400.png"
                         title="Disneyland Park"
-                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Go somewhere</Button>}
-                        attributes={[["Column 1", "Some longer information is here :)"], ["Column 2", "Information 2"], ["$39.99", "10.2 miles"]]}
-                        bidComponents={[
-                            <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
-                            </Row>,
-                            <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
-                            </Row>,
-                            <Button variant="light">+ Add Bid</Button>]}
-                    />
-                    <TripCard
-                        image="https://placeholder.co/600x400.png"
-                        title="Staples Center"
-                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Go somewhere</Button>}
-                        attributes={[["Column 1", "Information 1"], ["Column 2", "Information 2"], ["Column 3", "Information 3"]]}
-                        bidComponents={[]}
-                    />
-                    <TripCard
-                        image="https://placeholder.co/600x400.png"
-                        title="UCLA Jules Stein Eye Institute"
                         actionComponent={<DropdownButton as={ButtonGroup} title="Options" id="bg-nested-dropdown"
                             variant="light">
                             <Dropdown.Item>Cancel Trip</Dropdown.Item>
-                            <Dropdown.Item>Do Something</Dropdown.Item>
+                            <Dropdown.Item>Show More</Dropdown.Item>
                         </DropdownButton>}
-                        attributes={[["Column 1", "Information 1"], ["Column 2", "Information 2"], ["Column 3", "Information 3"]]}
-                        bidComponents={[]}
+                        attributes={[["Mon, Feb 19, 08:00 AM", "12 miles - 36 minutes"], ["Tim Pajim", "4.5\u2605 (130)"], ["$39.99", ""]]}
+                        bidComponents={[
+                            <Row>
+                                <Col
+                                     style={{ display: 'flex', justifyContent: 'center' }}> <span style={{ fontSize: "1.5em" }}>Rate Driver: &#x2606;&#x2606;&#x2606;&#x2606;&#x2606; </span>
+                                </Col>
+                            </Row>]}
                     />
                 </Row>
                 <div className="trips-header mt-4 mb-4">
@@ -60,18 +42,29 @@ function RiderTripsPage() {
                     <TripCard
                         image="https://placeholder.co/600x400.png"
                         title="Disneyland Park"
-                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Go somewhere</Button>}
-                        attributes={[["Column 1", "Some longer information is here :)"], ["Column 2", "Information 2"], ["$39.99", "10.2 miles"]]}
+                        actionComponent={<Button style={{ backgroundColor: "#D96C06" }}>Cancel Trip</Button>}
+                        attributes={[["Mon, Feb 19, 08:00 AM", "12 miles - 36 minutes"], ["Pickup:", "1234 Virginia Ave."], ["Destination:", "4567 Disney Rd."]]}
                         bidComponents={[
                             <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
+                                <Col> Tim Pajim <br /> 4.5&#9733; (130) </Col>
+                                <Col> <Button onClick={() => { setShowAddTrip(true) }} className="btn-add-trip"> $Price </Button> </Col>
                             </Row>,
                             <Row>
-                                <Col>Some left aligned components</Col>
-                                <Col className={"text-end"}>Some right aligned components</Col>
-                            </Row>,
-                            <Button variant="light">+ Add Bid</Button>]}
+                                <Col> Heavy but FREAKY <br /> 2.2&#9733; (34565)</Col>
+                                <Col> <Button onClick={() => { setShowAddTrip(true) }} className="btn-add-trip"> $Price </Button> </Col>
+                            </Row>]}
+                    />
+                </Row>
+                <div className="trips-header mt-4 mb-4">
+                    <h2>Archived Trips</h2>
+                </div>
+                <Row xs={1} md={2} lg={1}>
+                    <TripCard
+                        image="https://placeholder.co/600x400.png"
+                        title="Disneyland Park"
+                        actionComponent={""}
+                        attributes={[["Mon, Feb 19, 08:00 AM", "12 miles - 36 minutes"], ["Tim Pajim", "4.5\u2605 (130)"], ["$39.99", ""]]}
+                        bidComponents={[]}
                     />
                 </Row>
             </div>
