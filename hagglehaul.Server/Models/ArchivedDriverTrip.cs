20cc6278ba2c129edbb220ba31209c45d8681ebf
Jacob Zhi <54284925+jaczhi@users.noreply.github.com>
2024-02-27 11:23:06 -0800

    Add interactive maps to dashboards (#100)
    * Use leaflet to process and render the geojson stored for each geographic route

--- hagglehaul.Server/Models/ArchivedDriverTrip.cs
+++ hagglehaul.Server/Models/ArchivedDriverTrip.cs
@@ -5,6 +5,7 @@
         public string TripId { get; set; } = null!;
         public string TripName { get; set; } = null!;
         public byte[] Thumbnail { get; set; } = null!;
+        public string GeoJson { get; set; } = null!;
         public DateTime StartTime { get; set; }
         public double? Distance { get; set; } //distance in meters
         public double? Duration { get; set; } //duration in seconds
