d946ffe23ee15fd73888762c271af532e6752aac
CE201710 <148310909+CE201710@users.noreply.github.com>
2024-02-26 17:13:27 -0800

    Add files via upload

--- hagglehaul.Server/EmailViews/ConfirmationEmail.cshtml.cs
+++ hagglehaul.Server/EmailViews/ConfirmationEmail.cshtml.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace hagglehaul.Server.EmailViews;
-
-public class ConfirmationEmail : PageModel
+namespace hagglehaul.Server.EmailViews
 {
-    public string ExampleProperty { get; set; }
+    public class ConfirmationEmail
+    {
+        public string RiderName { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+        public string DriverName { get; set; }
+        public decimal Price { get; set; }
+    }
 }
