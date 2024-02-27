1e6157039546fc4bd49fadee5173ad93ebc72a98
CE201710 <148310909+CE201710@users.noreply.github.com>
2024-02-26 17:53:28 -0800

    Add files via upload

--- hagglehaul.Server/EmailViews/AcceptedBidEmail.cshtml.cs
+++ hagglehaul.Server/EmailViews/AcceptedBidEmail.cshtml.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace hagglehaul.Server.EmailViews;
-
-public class AcceptedBidEmail : PageModel
+namespace hagglehaul.Server.EmailViews
 {
-    public string ExampleProperty { get; set; }
+    public class AcceptedBidEmail
+    {
+        public string RiderName { get; set; }
+
+        public string DriverName { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+        public decimal Price { get; set; }
+        
+        
+    }
 }
