4f7992fec6c85741e009cf387ef4d5c30e372e42
CE201710 <148310909+CE201710@users.noreply.github.com>
2024-03-01 23:45:02 -0800

    Email Views for Rider and Driver (#98)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update AcceptedBidEmail.cshtml
    
    * Update NewBidEmail.cshtml
    
    * Update NewBidEmail.cshtml.cs
    
    * polishing
    
    ---------
    
    Co-authored-by: Teddy1405 <48705917+Teddy1405@users.noreply.github.com>

--- hagglehaul.Server/EmailViews/ConfirmationEmail.cshtml.cs
+++ hagglehaul.Server/EmailViews/ConfirmationEmail.cshtml.cs
@@ -1,8 +1,23 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace hagglehaul.Server.EmailViews;
-
-public class ConfirmationEmail : PageModel
+namespace hagglehaul.Server.EmailViews
 {
-    public string ExampleProperty { get; set; }
+    public class ConfirmationEmail
+    {
+        public string RiderName { get; set; }
+
+        public string TripName { get; set; }
+        public string DriverName { get; set; }
+        public double DriverRating { get; set; }
+        public decimal Price { get; set; } // In dollars
+
+        public string DriverPhone { get; set; }
+        public string DriverEmail { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public string PickupAddress { get; set; }
+        public string DestinationAddress { get; set; }
+
+        public string RiderEmail { get; set; }
+    }
 }
--- hagglehaul.Server/EmailViews/NewBidEmail.cshtml.cs
+++ hagglehaul.Server/EmailViews/NewBidEmail.cshtml.cs
@@ -1,8 +1,18 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace hagglehaul.Server.EmailViews;
-
-public class NewBidEmail : PageModel
+namespace hagglehaul.Server.EmailViews
 {
-    public string ExampleProperty { get; set; }
+    public class NewBidEmail
+    {
+        public string RiderName { get; set; }
+
+        public string TripName { get; set; }
+        public string DriverName { get; set; }
+        public double DriverRating { get; set; }
+        public decimal Price { get; set; } // In dollars
+
+        public DateTime StartTime { get; set; }
+        public string PickupAddress { get; set; }
+        public string DestinationAddress { get; set; }
+
+        public string RiderEmail { get; set; }
+    }
 }
