f4456c9fc24f8559947ada4e9c28f6b2bf457299
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-03-07 00:27:27 -0800

    endpoint parameter documentation

--- hagglehaul.Server/Models/AddTripDriver.cs
+++ hagglehaul.Server/Models/AddTripDriver.cs
@@ -1,7 +1,13 @@
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace hagglehaul.Server.Models;
 
+[SwaggerSchema("Form to confirm a driver for a trip")]
 public class AddTripDriver
 {
+    [SwaggerSchema("The trip ID to confirm the driver on")]
     public string TripId { get; set; }
+
+    [SwaggerSchema("The bid ID to verify for the trip")]
     public string BidId { get; set; }
 }
--- hagglehaul.Server/Models/DriverUpdate.cs
+++ hagglehaul.Server/Models/DriverUpdate.cs
@@ -1,11 +1,23 @@
-﻿namespace hagglehaul.Server.Models
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace hagglehaul.Server.Models
 {
+    [SwaggerSchema("Form to update a driver's information")]
     public class DriverUpdate
     {
+        [SwaggerSchema("Name of the driver")]
         public string Name { get; set; }
+
+        [SwaggerSchema("Phone number of the driver")]
         public string Phone { get; set; }
+
+        [SwaggerSchema("Description of the driver's car")]
         public string CarDescription { get; set; }
+
+        [SwaggerSchema("Current password of the driver")]
         public string CurrentPassword { get; set; }
+
+        [SwaggerSchema("New password of the driver")]
         public string NewPassword { get; set; }
     }
 }
